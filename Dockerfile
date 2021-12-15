3356f54c43da7b69d40d466460ca3f63ab4a6373
aisaacson <aisaacson@newrelic.com>
2021-12-15 16:11:31 -0500

    update entrypoint
    This is my description

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM newrelic/cli:latest
 
+RUN apk update \
+ && apk add jq \
+ && rm -rf /var/cache/apk/*
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 # COPY entrypoint.sh /entrypoint.sh
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+nrEventType=pushEvent
+
+eventJSON=$(jq -n \
+               --arg eventType "$nrEventType" \
+               --arg user "${NEW_RELIC_DEPLOYMENT_USER}" \
+               --arg revision "${NEW_RELIC_DEPLOYMENT_REVISION}" \
+               --arg path "${PATH}" \
+               '{eventType: $eventType, user: $user, revision: $revision, path: $path}' )
+
+echo $eventJSON
+
 result=$(newrelic events post \
-  --accountId "${NEW_RELIC_ACCOUNT_ID}" \
-  --event '{ "eventType": "pushEvent", "user": "${NEW_RELIC_DEPLOYMENT_USER}", "revision": "${NEW_RELIC_DEPLOYMENT_REVISION}" }'
+  --event "$eventJSON" \
   2>&1)
 
 exitStatus=$?
