5f622ada78606a76151e5da10dbab569ac2c8e1b
aisaacson <aisaacson@newrelic.com>
2021-12-20 14:32:32 -0500

    More attributes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,10 @@ eventJSON=$(jq -n \
                --arg eventType "$nrEventType" \
                --arg user "${NEW_RELIC_DEPLOYMENT_USER}" \
                --arg repository "${GITHUB_REPOSITORY}" \
-               --arg githubSHA "${GITHUB_SHA}" \
+               --arg commitHash "${GITHUB_SHA}" \
                --arg repoOwner "${GITHUB_REPOSITORY_OWNER}" \
-               '{eventType: $eventType, user: $user, repository: $repository, githubSHA: $githubSHA, repoOwner: $repoOwner}' )
+               --arg branch "${GITHUB_REF}"
+               '{eventType: $eventType, user: $user, repository: $repository, commitHash: $githubSHA, repoOwner: $repoOwner, branch: $branch}' )
 
 echo $eventJSON
 
